a8d7533f238979e6911606bb849a0a16895a9378
Alejandro Ramírez <ale_rm96@outlook.com>
2024-04-11 18:00:19 -0600

    modificacion de productos

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -42,4 +42,23 @@ function scrollToTop() {
     });
   });
 
+// Seccion de productos
 
+function openModal() {
+
+  document.getElementById("myModal").style.display = "block";
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == document.getElementById("myModal")) {
+    document.getElementById("myModal").style.display = "none";
+  }
+}
